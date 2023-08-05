dc513112a0d42514b2069bc9576afe0b7a61b9d1
Liskie <wyj125869@126.com>
2023-08-05 10:17:47 +0800

    feat: Added progress bar and statistics in the data cleaner.

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,6 @@
 import fire
 from jsonlines import jsonlines
+from rich.progress import track
 
 
 class DataCleaner:
@@ -13,13 +14,22 @@ class DataCleaner:
             self.output_path = output_path
 
     def clean(self):
+        total = 0
+        valid = 0
+
         with (jsonlines.open(self.input_path, 'r') as reader):
             with jsonlines.open(self.output_path, 'w') as writer:
-                for datapoint in reader:
+                for datapoint in track(reader, description='Cleaning datapoints: '):
+                    total += 1
+
                     if datapoint['zh']['question'].startswith('<error>') or \
                             datapoint['zh']['response'].startswith('<error>'):
                         continue
+
+                    valid += 1
                     writer.write(datapoint)
+
+        print(f'Cleaning completed. {valid} / {total} = {valid / total:.2f}% datapoints are valid.')
 
 
 if __name__ == '__main__':
