08832b1f366ef76639caedd974d9e565f75bc345
Liskie <wyj125869@126.com>
2023-08-07 16:31:00 +0800

    feat: Adjusted the format of the sample output to json.

--- sampler.py
+++ sampler.py
@@ -28,8 +28,7 @@ class OrcaSampler:
                     sampled_lines.append(line)
 
         with open(self.sample_file_path, 'w') as writer:
-            for line in sampled_lines:
-                writer.write(f'{json.dumps(line, indent=4, ensure_ascii=False)}\n')
+            json.dump(sampled_lines, writer, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
