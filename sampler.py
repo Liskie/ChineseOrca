895c576ab3b83655bbd23bd04b9923192ab116ea
Liskie <wyj125869@126.com>
2023-08-09 10:32:38 +0800

    feat: Added sample_by_system_prompt in the sampler.

--- sampler.py
+++ sampler.py
@@ -1,5 +1,6 @@
 import json
 import random
+from collections import defaultdict
 
 import fire
 from jsonlines import jsonlines
@@ -39,6 +40,31 @@ class OrcaSampler:
             json.dump(sampled_lines, writer, indent=4, ensure_ascii=False)
 
         print(f'Dumped {num} lines to {self.sample_file_path}.')
+
+    def sample_by_system_prompt(self, num_per_prompt: int = 5, start: int = 0, end: int = -1) -> None:
+        # Calculate the number of lines in the file
+        with jsonlines.open(self.input_file_path, 'r') as reader:
+            num_lines = sum(1 for _ in reader)
+
+        if end == -1:
+            end = num_lines
+
+        system_prompt2samples = defaultdict(list)
+
+        with jsonlines.open(self.input_file_path, 'r') as reader:
+            for i, line in enumerate(reader):
+                if start <= i < end:
+                    system_prompt2samples[line['system_prompt']].append(line)
+
+        sampled_lines = []
+        for system_prompt, samples in system_prompt2samples.items():
+            sampled_lines.extend(random.sample(samples, num_per_prompt))
+
+        with open(self.sample_file_path, 'w') as writer:
+            json.dump(sampled_lines, writer, indent=4, ensure_ascii=False)
+
+        print(f'Dumped {num_per_prompt} * {len(system_prompt2samples)} = {num_per_prompt * len(system_prompt2samples)} '
+              f'lines to {self.sample_file_path}.')
 
 
 if __name__ == '__main__':
