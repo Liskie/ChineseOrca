277230fdf465c7add5ddbcbc5018ad456374f5d1
Liskie <wyj125869@126.com>
2023-08-10 16:27:03 +0800

    feat: Added data_filters.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,5 @@
 from .data_buffer import *
 from .data_structure import *
 from .functions import *
+from .speedometer import *
+from .data_filters import *
---
+++ utils/data_filters.py
@@ -0,0 +1,10 @@
+from .data_structure import Datapoint
+
+
+def pre_question_translation_filter(datapoint: Datapoint):
+    pass
+
+
+def post_question_translation_filter(datapoint: Datapoint):
+    pass
+
